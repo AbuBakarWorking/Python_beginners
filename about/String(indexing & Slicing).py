1f7be47d8ad52559213e80c69d28c1f9193fb6d7
AbuBakarWorking <abubakarshapatar001@gmail.com>
2025-03-14 20:06:04 +0500

    Indexing & Slicing

---
+++ about/String(indexing & Slicing).py
@@ -0,0 +1,35 @@
+print("String Indexing and Slicing")
+
+print("\nString Indexing")
+
+s = "Abubakar is a boy"
+w = "Welcome to Python Programming"
+
+print("\nIf you want to count from left then count from 0 value as index")
+print(s)
+print(s[0]) # A
+print(s[10]) # b
+print(w[1:15:2]) 
+
+
+
+
+
+print("\nIf you want to count from right then count from -1 value as index")
+print(s[-1]) # y
+
+
+print("\nString Slicing")
+print(w)
+print(w[0:5])
+print(w[0:10])
+
+print("\nString Slicing in reverse order")
+print(w[-1::-2]) #reverse order
+print(w[-1:-15:-2])
+print(w[-1:-15:-1])
+
+print(w[1:15:1]) # order wise
+
+
+
